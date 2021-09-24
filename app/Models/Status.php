2104513a3576a55f42c0e506a2501abef00f4971
hantao <937626747@qq.com>
2021-09-24 09:26:08 +0800

    发布微博

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -9,6 +9,8 @@ class Status extends Model
 {
     use HasFactory;
 
+    protected $fillable = ['user_id', 'content'];
+
     public function user()
     {
         return $this->belongsTo(User::class);
